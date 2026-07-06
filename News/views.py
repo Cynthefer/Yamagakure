4033e4a80718990e87f917798e09636b9d01ab14
Cynthefer <cynthefer@gmail.com>
2026-07-06 23:55:21 +0200

    Base Commit
    - Make Templates next
    - Research the template directory detection next to place it better
    
    - Cynthefer/Ei

--- News/views.py
+++ News/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
 # Create your views here.
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 6
+    template_name='news/post/list.html'
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    return render(request,
+                  'news/post/detail.html',
+                  {'post':post})
